ba1c8f80b1667081ff4c623ad8dd439989f983e4
essss <sousououederni@gmail.com>
2024-11-15 01:12:46 +0200

    How it works section

--- suncityla/app/page.tsx
+++ suncityla/app/page.tsx
@@ -1,10 +1,15 @@
 import Hero from "./components/Hero";
+import HowItWorks from "./components/How It Works/HowItWorks";
 
 export default async function Home() {
   return (
     <>
       <Hero/>
-      
+      <section className=" flex flex-col justify-center items-center mt-14 mb-14">
+        <h2 className="text-3xl font-semibold mb-6 text-center heading">About</h2>
+        <p className="text-lg text-black w-[600px]">SunCityLA is project that provides a free service where a city-hired specialist  evaluates the installation and maintenance costs of solar panels.</p>
+      </section>
+      <HowItWorks/>
     </>
   );
 }
