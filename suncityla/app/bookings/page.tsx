dc4bbf2a925e61e39309bb290c7d385e0d59a1ec
fmtabbara <fmtabbara@hotmail.co.uk>
2024-11-20 11:48:12 +0000

    make bookings readable

--- suncityla/app/bookings/page.tsx
+++ suncityla/app/bookings/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import getBookings from "./actions/getBookings";
 import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
 
 export const dynamic = "force-dynamic";
 
@@ -10,7 +11,17 @@ export default async function BookingsPage() {
   return (
     <div>
       <h1>Bookings</h1>
-      {JSON.stringify(bookings, null, 2)}
+      {bookings.map((booking) => (
+        <div key={booking.id} className="p-2 border mb-2 w-80">
+          <h2>{booking.firstname}</h2>
+          <p>{booking.lastname}</p>
+          <p>{format(new Date(booking.bookingDate), "EE dd/MM/yyyy")}</p>
+          <p>{booking.streetAddress}</p>
+          <p>{booking.postalCode}</p>
+          <p>{booking.state}</p>
+          <p>{booking.status}</p>
+        </div>
+      ))}
       <Link href="/bookings/new">
         <Button>New Booking</Button>
       </Link>
