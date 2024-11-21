6e2ac5eb812d7e83ec584dd2450e0f4c986112e8
samad13 <yusufabdulsamad93@gmail.com>
2024-11-21 21:30:10 +0100

    verifying the id

--- suncityla/app/bookings/actions/getBookings.ts
+++ suncityla/app/bookings/actions/getBookings.ts
@@ -2,7 +2,7 @@
 
 import prisma from "@/prisma/prismaClient";
 
-const getBookings = async (bookingId: string) => {
+const getBookings = async () => {
   const bookings = await prisma.booking.findMany();
   return bookings;
 };
