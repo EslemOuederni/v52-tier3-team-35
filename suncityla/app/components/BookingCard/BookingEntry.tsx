70408fa027dcdfa46100675c8303715bdfe506dd
fmtabbara <fmtabbara@hotmail.co.uk>
2024-12-02 23:56:40 +0000

    show booking reference

--- suncityla/app/components/BookingCard/BookingEntry.tsx
+++ suncityla/app/components/BookingCard/BookingEntry.tsx
@@ -12,31 +12,38 @@ import { format } from 'date-fns';
 
 const BookingEntry = ({ booking }: { booking: Booking }) => {
   return (
-    <Table>
-      <TableCaption>Your booking</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Firstname</TableHead>
-          <TableHead>Lastname</TableHead>
-          <TableHead>Booking date</TableHead>
-          <TableHead>Address line 1</TableHead>
-          <TableHead>Address line 2</TableHead>
-          <TableHead>State</TableHead>
-          <TableHead>Booking status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>{booking.firstname}</TableCell>
-          <TableCell>{booking.lastname}</TableCell>
-          <TableCell>{format(new Date(booking.bookingDate), 'yyyy-MM-dd')}</TableCell>
-          <TableCell>{booking.streetAddress}</TableCell>
-          <TableCell>{booking.postalCode}</TableCell>
-          <TableCell>{booking.state}</TableCell>
-          <TableCell>{booking.status}</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div>
+      <div className="flex-row justify-center">
+        <div className=" mb-4 font-bold bg-yellow-200 border border-yellow-500 px-6 py-4 rounded-md">
+          Your booking reference is {booking.id}
+        </div>
+      </div>
+      <Table>
+        <TableCaption>Your booking</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Firstname</TableHead>
+            <TableHead>Lastname</TableHead>
+            <TableHead>Booking date</TableHead>
+            <TableHead>Address line 1</TableHead>
+            <TableHead>Address line 2</TableHead>
+            <TableHead>State</TableHead>
+            <TableHead>Booking status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>{booking.firstname}</TableCell>
+            <TableCell>{booking.lastname}</TableCell>
+            <TableCell>{format(new Date(booking.bookingDate), 'yyyy-MM-dd')}</TableCell>
+            <TableCell>{booking.streetAddress}</TableCell>
+            <TableCell>{booking.postalCode}</TableCell>
+            <TableCell>{booking.state}</TableCell>
+            <TableCell>{booking.status}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
