1e132855df7059b3636a4f01e45713d715a055b4
essss <sousououederni@gmail.com>
2024-12-11 16:30:13 +0200

    updated ui

--- suncityla/app/booking/new/page.tsx
+++ suncityla/app/booking/new/page.tsx
@@ -1,9 +1,9 @@
 import BookingForm from '../components/BookingForm/BookingForm';
 
-export default function NewBooking() {
+export default function NewBooking () {
   return (
-    <div>
-      <h1>New Booking</h1>
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className=' font-bold mb-4'>New Booking</h1>
       <BookingForm />
     </div>
   );
